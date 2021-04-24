4bdef9a4f3dff4c0901e4956000dfea6df357c0d
sergey.binder <fornewreal@gmail.com>
2021-04-24 11:20:12 +0700

    Basics for leechers

---
+++ Assets/Scripts/TentacleTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace LD48Project {
+	public sealed class TentacleTest : MonoBehaviour {
+		public Transform ForceApplyPos;
+		public float     Force;
+		public Transform Target;
+
+		Rigidbody2D _rigidbody;
+
+		void Start() {
+			_rigidbody = GetComponent<Rigidbody2D>();
+		}
+
+		void Update() {
+			if ( Input.GetKey(KeyCode.Space) ) {
+				_rigidbody.AddForceAtPosition((Target.position - ForceApplyPos.position).normalized * Force,
+					ForceApplyPos.position, ForceMode2D.Force);
+			}
+		}
+	}
+}
