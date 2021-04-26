b43be3431d7cb30f3cbd5fb181ad96e112ce6789
sergey.binder <fornewreal@gmail.com>
2021-04-26 23:17:03 +0700

    Added depth meter

---
+++ Assets/Scripts/DepthController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+using System.Globalization;
+
+using TMPro;
+
+namespace LD48Project {
+	public sealed class DepthController : MonoBehaviour {
+		[Header("Parameters")]
+		public float DescendSpeed;
+		[Header("Dependencies")]
+		public TMP_Text DepthText;
+
+		public float CurDepth { get; private set; } = 0f;
+
+		void Update() {
+			if ( !Mathf.Approximately(DescendSpeed, 0f) ) {
+				CurDepth += DescendSpeed * Time.deltaTime;
+				UpdateText();
+			}
+		}
+
+		void UpdateText() {
+			DepthText.text = $"Depth: {CurDepth.ToString("F2", CultureInfo.InvariantCulture)}m";
+		}
+	}
+}
